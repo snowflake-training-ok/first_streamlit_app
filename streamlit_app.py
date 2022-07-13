e0691e34528a75aa05f07063ef4bfded9ab8e7a5
snowflake-training-ok <109235694+snowflake-training-ok@users.noreply.github.com>
2022-07-13 12:45:36 -0400

    updated emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(st.markdown(":bowl_with_spoon:")'Omega 3 & Blueberry Oatmeal')
-streamlit.text(st.markdown(":green_salad:")'Kale, Spinach & Rocket Smoothie')
-streamlit.text(st.markdown(":chicken:")'Hard-Boiled Free-Range Egg')
-streamlit.text(st.markdown(":avocado:")st.markdown(":bread:")'Avacado Toast')
+st.markdown(":bowl_with_spoon:") streamlit.text('Omega 3 & Blueberry Oatmeal')
+st.markdown(":green_salad:") streamlit.text('Kale, Spinach & Rocket Smoothie')
+st.markdown(":chicken:") streamlit.text('Hard-Boiled Free-Range Egg')
+st.markdown(":avocado:")st.markdown(":bread:") streamlit.text('Avacado Toast')
