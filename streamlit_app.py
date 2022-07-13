7e5597ef5a2de5ae15758f096b36bb3ed5a2aa18
snowflake-training-ok <109235694+snowflake-training-ok@users.noreply.github.com>
2022-07-13 12:39:14 -0400

    Changed header text and added Emojis to the menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text(:bowl_with_spoon: 'Omega 3 & Blueberry Oatmeal')
+streamlit.text(:green_salad: 'Kale, Spinach & Rocket Smoothie')
+streamlit.text(:chicken: 'Hard-Boiled Free-Range Egg')
+streamlit.text(:avocado::bread: 'Avacado Toast')
