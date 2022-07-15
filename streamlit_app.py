b4eb424a2cc6f6d3690f195e3c9d4332c7409880
snowflake-training-ok <109235694+snowflake-training-ok@users.noreply.github.com>
2022-07-15 15:10:14 -0400

    Add the Fruit Name Submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 
 except URLError as e:
   streamlit.error
-
+  
 #import requests
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -56,9 +56,21 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+#allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding "+new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like ot add?')
+if streamlit.button('Add a fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
 #don't run anything past here while we troubleshoot
-streamlit.stop() 
-  
+#streamlit.stop() 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screeen
 # take the json version of the response and normalize it
